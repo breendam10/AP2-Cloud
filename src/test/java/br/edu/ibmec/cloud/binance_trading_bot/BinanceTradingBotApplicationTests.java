4092736e03ebd1b6b527922fd717a0d483955916
Esther <eympessanha@gmail.com>
2025-06-03 23:31:31 -0300

    subindo base da ap2

---
+++ src/test/java/br/edu/ibmec/cloud/binance_trading_bot/BinanceTradingBotApplicationTests.java
@@ -0,0 +1,15 @@
+package br.edu.ibmec.cloud.binance_trading_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import br.edu.ibmec.cloud.tradingbot.BinanceTradingBotApplication;
+
+@SpringBootTest(classes = BinanceTradingBotApplication.class)
+class BinanceTradingBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
